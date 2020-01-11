e84e6e3475f84e1e6e62ae027a324ae7499a98e6
rahul215 <rahul.saxena_cs18@gla.ac.in>
2020-01-11 07:15:22 -1200

    Initial commit

---
+++ src/Repeat.java
@@ -0,0 +1,13 @@
+public class Repeat {
+    public static void main(String[] args) {
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+        System.out.println("Float minimum value =" + myMinFloatValue);
+        System.out.println("Float maximum value =" + myMaxFloatValue);
+
+        double Mymindoublevalue = Double.MIN_VALUE;
+        double Mymaxdoublevalue = Double.MAX_VALUE;
+        System.out.println("Double min value = " + Mymindoublevalue);
+        System.out.println("Double max value = " + Mymaxdoublevalue);
+    }
+}
